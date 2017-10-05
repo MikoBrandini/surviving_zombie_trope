a7ec9be30edca91ebe927c044a1b4fc950ea8416
Miko Brandini <MikoBrandini@gmail.com>
2017-10-05 00:27:12 -0400

    Added basic outline of narratives for each path possible in the game.

--- src/paths.js
+++ src/paths.js
@@ -3,7 +3,7 @@ const paths = [{
         "text": "A bizzare case of rabies is spreading throughout the city. People inflected with the disease seem to perish but rise up and begin feed on the living. You're starting to slightly worry and it doesn't help that you're not feeling well.  ",
         "img": "https://i.imgur.com/iFuJQiN.jpg",
         "paths": [{
-                "buttonTitle": "Go upstairs.",
+                "buttonTitle": "Visit Friend",
                 "next": 1
             },
             {
@@ -11,7 +11,7 @@ const paths = [{
                 "next": 4
             },
             {
-                "buttonTitle": "Go to the cafeteria.",
+                "buttonTitle": "Take Train To Countryside",
                 "next": 2
             },
             {
@@ -24,14 +24,14 @@ const paths = [{
 
     {
         "id": 1,
-        "text": "You find the locker room and decide to take a shower. After drying up, you can hear a young man begging for help.",
-        "img": "https://i.imgur.com/xPbxlei.jpg",
+        "text": "You decide to contact your friend to ride out apocalypse together. He is well armed and ready to kick some zombie ass. Maybe a little too much for your liking. He wants to go to the mall and stock up on some ammo before it's all gone.",
+        "img": "https://i.imgur.com/EIZwLBi.jpg?1",
         "paths": [{
-                "buttonTitle": "Go back to where you woke up.",
+                "buttonTitle": "Go back home.",
                 "next": 0
             },
             {
-                "buttonTitle": "Find the young man in peril. ",
+                "buttonTitle": "Go To The Mall",
                 "next": 5
             },
         ]
@@ -39,14 +39,14 @@ const paths = [{
 
     {
         "id": 2,
-        "text": "You enter the empty cafeteria, but from the kitchen you hear strange sounds.",
-        "img": "https://mikobrandini.github.io/portfolio/assets/img/mikotv.png",
+        "text": "It seems like you weren't the only person with that idea. The trains are stil running, but slowly. After pushing yourself to the platform, you can hear the train approaching. Unfortunately at the same time, the infection is spreading somewhere in the back. You can either wait for the upcoming train or take an emergency exit and run back home. ",
+        "img": "https://i.imgur.com/pASSAoI.jpg?1",
         "paths": [{
-                "buttonTitle": "Run back to where you came from.",
+                "buttonTitle": "Emergency Exit Home",
                 "next": 0
             },
             {
-                "buttonTitle": "Slowly approach the kitchen.",
+                "buttonTitle": "Wait For Train",
                 "next": 6
             },
 
@@ -66,13 +66,13 @@ const paths = [{
 
     {
         "id": 5,
-        "text": "You find the young man having his guts ripped open. The zombies see you and target you as well.",
-        "img": "https://i.imgur.com/xPbxlei.jpg"
+        "text": "You arrive and after fighting other survivors, you end up with the ammo. Sadly, your friend gets bitten. He bravely eliminates himself in order not to infect you. Shortly thereafter you get bit and turn into a zombie.",
+        "img": "https://i.imgur.com/aUJ62Yx.jpg?1"
     },
     {
         "id": 6,
-        "text": "As you enter the kitchen, a hoard of reanimated cafeteria workers surround and consume you. ",
-        "img": "https://i.imgur.com/xPbxlei.jpg"
+        "text": "While waititng for the train, you see a zombie jump on a woman and begin to infect her. A young girl, possibly her daughter watches in horror. The train arrives and while being shoved inside by other survivors, you decide to take the traumatized girl with you. Right as you pick her up, you hear a new victim being infected right next you. Unfortunately some of their blood lands in open mouth. You and the girl reach the outskirts of the city, right as you begin to turn into a threat for everyone in the subway car.",
+        "img": "https://i.imgur.com/FfnevrJ.jpg?1"
     },
 
 
